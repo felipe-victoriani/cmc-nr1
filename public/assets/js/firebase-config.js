7b693745f3868637e862857452a8e20d1d23cd95
Carlos Felipe Lima Victoriani <felipevictoriani@hotmail.com>
2026-04-15 21:44:26 -0400

    feat: add firebase-config.js for Vercel deploy

---
+++ public/assets/js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBVWfEQLJ-y0VopfLpmy6mQUBPbIw9BtOM",
+  authDomain: "cmv-nr1.firebaseapp.com",
+  databaseURL: "https://cmv-nr1-default-rtdb.firebaseio.com",
+  projectId: "cmv-nr1",
+  storageBucket: "cmv-nr1.firebasestorage.app",
+  messagingSenderId: "682823321669",
+  appId: "1:682823321669:web:398f558f8fb64959c03b2c",
+};
